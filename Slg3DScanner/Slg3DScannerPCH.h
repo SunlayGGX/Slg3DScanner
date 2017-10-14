f2246a7114fb69bf7f59df738363a5906f915451
SunlayGGX <sunlay.g@free.fr>
2017-10-14 14:11:31 -0400

    Added SlgLogger dependency to core application

--- Slg3DScanner/Slg3DScannerPCH.h
+++ Slg3DScanner/Slg3DScannerPCH.h
@@ -21,11 +21,14 @@
 
 //My own
 #include "Slg3DScannerHelperPCH.h"
+#include "LoggerPCH.h"
 
 
 //Lib inclusion
 #ifdef _DEBUG
 #   pragma comment(lib, "Slg3DScannerHelper_d.lib")
+#   pragma comment(lib, "SlgLogger_d.lib")
 #else
 #   pragma comment(lib, "Slg3DScannerHelper.lib")
+#   pragma comment(lib, "SlgLogger.lib")
 #endif
