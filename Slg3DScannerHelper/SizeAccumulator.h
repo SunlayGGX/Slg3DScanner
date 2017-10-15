b7e679f360622467134bc62d2fe8417806830f92
SunlayGGX <sunlay.g@free.fr>
2017-10-14 21:49:10 -0400

    Update SizeAccumulator to MoonRPG SizeAccumulator version

--- Slg3DScannerHelper/SizeAccumulator.h
+++ Slg3DScannerHelper/SizeAccumulator.h
@@ -3,23 +3,37 @@
 
 namespace Slg3DScanner
 {
-    template<class Type, class ... Args>
+    /*
+    Meta object to count the size of all elements.
+    */
+    template<class FirstType, class ... OtherType>
     struct SizeAccumulator
     {
     public:
         enum
         {
-            value = sizeof(Type) + SizeAccumulator<Args...>::value
+            value = sizeof(FirstType) + SizeAccumulator<OtherType...>::value,
+
+            cache_optimized_value = SizeAccumulator<FirstType>::cache_optimized_value + SizeAccumulator<OtherType...>::cache_optimized_value
         };
     };
 
-    template<class Type>
-    struct SizeAccumulator<Type>
+    /*
+    Version with one element
+    */
+    template<class LonelyType>
+    struct SizeAccumulator<LonelyType>
     {
     public:
         enum
         {
-            value = sizeof(Type)
+            value = sizeof(LonelyType),
+
+            remainingOnCacheLine = value % SYSTEM_CACHE_ALIGNMENT_SIZE,
+            isCacheLineEqual = remainingOnCacheLine == 0,
+            toAddToCompleteCacheLine = isCacheLineEqual ? 0 : SYSTEM_CACHE_ALIGNMENT_SIZE - remainingOnCacheLine,
+
+            cache_optimized_value = value + toAddToCompleteCacheLine
         };
     };
 }
