b352359561b864d1db9cb724648b7342512e143b
SunlayGGX <sunlay.g@free.fr>
2017-10-16 02:22:55 -0400

    Added SlgCustomExtension project to solution

---
+++ SlgCustomExtension/SlgCustomExtensionPCH.cpp
@@ -0,0 +1,1 @@
+#include "SlgCustomExtensionPCH.h"
---
+++ SlgCustomExtension/SlgCustomExtensionPCH.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+//Standard
+#include <string>
+#include <memory>
+#include <vector>
+#include <stdexcept>
+#include <cassert>
+
+
+//My own
+#include "Slg3DScannerHelperPCH.h"
+#include "LoggerPCH.h"
+
+
+//Lib inclusion
+#ifdef _DEBUG
+#   pragma comment(lib, "Slg3DScannerHelper_d.lib")
+#   pragma comment(lib, "SlgLogger_d.lib")
+#else
+#   pragma comment(lib, "Slg3DScannerHelper.lib")
+#   pragma comment(lib, "SlgLogger.lib")
+#endif
+
