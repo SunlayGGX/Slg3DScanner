2ea61c73554882f750c075b394f247bc67129e34
SunlayGGX <sunlay.g@free.fr>
2017-10-17 00:37:28 -0400

    Added RessourceManager

---
+++ SlgRessource/SlgRessourceManager.cpp
@@ -0,0 +1,22 @@
+#include "SlgRessourcePCH.h"
+#include "SlgRessourceManager.h"
+
+using namespace Slg3DScanner;
+
+RessourceManager::RessourceManager()
+{
+}
+
+RessourceManager::~RessourceManager()
+{
+}
+
+void RessourceManager::initialize()
+{
+
+}
+
+void RessourceManager::destroy()
+{
+
+}
---
+++ SlgRessource/SlgRessourceManager.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SlgSingleton.h"
+
+namespace Slg3DScanner
+{
+    class RessourceManager : private Slg3DScanner::SlgSingleton<RessourceManager>
+    {
+    private:
+        SLGENGINE_GENERATE_CODE_FROM_SlgSingleton(RessourceManager);
+
+
+    private:
+
+
+
+    public:
+        virtual void initialize() override;
+        virtual void destroy() override;
+    };
+}
