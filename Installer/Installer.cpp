427089c4372079c94b33c925b7b7324024b318b4
SunlayGGX <sunlay.g@free.fr>
2017-10-20 00:45:03 -0400

    Installation main finished.

--- Installer/Installer.cpp
+++ Installer/Installer.cpp
@@ -1,6 +1,52 @@
+#include "SlgDevEnvBind.h"
 
-int main()
+namespace Slg3DScanner
 {
-    return 0;
+    namespace SlgTools
+    {
+        void logInstallationFailed()
+        {
+            LOG("\n\n****************");
+            LOG("Cancellation complete!");
+            LOG("\nInstallation failed!\n");
+        }
+
+        void logInstallationSuccess()
+        {
+            LOG("\n\n****************");
+            LOG("Operation successful!");
+            LOG("\nInstallation complete!\n");
+        }
+    }
 }
 
+int main() try
+{
+    LOG("*******************************************");
+    LOG("*** SETTING ENVIRONMENT VARIABLES PATHS ***");
+    LOG("*******************************************");
+
+    Slg3DScanner::SlgTools::linkSlgEnvironmentVariableToFolder(
+        SLG_ENVIRONMENT_VARIABLE_BIND(USER, "SLG_DEVEL", "Slg3DScanner"),
+        SLG_ENVIRONMENT_VARIABLE_BIND(USER, "SLG_DEPEND", "SlgDependencies")
+    );
+
+    Slg3DScanner::SlgTools::logInstallationSuccess();
+
+    system("pause");
+    return 0;
+}
+catch(const std::exception &e)
+{
+    Slg3DScanner::SlgTools::logInstallationFailed();
+
+    ::MessageBoxA(nullptr, e.what(), "Error", MB_ICONERROR);
+    return -99;
+}
+catch(...)
+{
+    Slg3DScanner::SlgTools::logInstallationFailed();
+
+    ::MessageBoxA(nullptr, "Unknown error", "Error", MB_ICONERROR);
+    return -100;
+}
