7bfb99105c7e30a00689c99f25cf82f7eacaa6c8
SunlayGGX <sunlay.g@free.fr>
2017-10-23 03:15:51 -0400

    Added an object for computing normals. Not implemented yet because it is 3.15 am and I'm sleepy...

---
+++ SlgSurfaceComputation/NormalComputator.cpp
@@ -0,0 +1,12 @@
+#include "SlgSurfaceComputationPCH.h"
+#include "NormalComputator.h"
+
+using namespace Slg3DScanner;
+
+NormalComputator::NormalComputator()
+{
+}
+
+NormalComputator::~NormalComputator()
+{
+}
---
+++ SlgSurfaceComputation/NormalComputator.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+namespace Slg3DScanner
+{
+    class NormalComputator
+    {
+    public:
+        NormalComputator();
+        ~NormalComputator();
+    };
+}
